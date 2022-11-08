c7b60e40586e351ecc01879a748e25eef74ec537
Guilherme Castello <guilherme.brasolin@outlook.com>
2022-11-08 01:58:45 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from 'react-router-dom'
+import Login from '../src/pages/Login'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<Login></Login>} />
+      </Routes>
+    </div>    
+  )
+}
+
+export default App
