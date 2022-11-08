698d216c7f92367c85e1579fbb6d1aac0054b4ed
Guilherme Castello <guilherme.brasolin@outlook.com>
2022-11-08 04:02:36 -0300

    starting to build login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,8 @@
-export default function Login () {
-    return <div>1</div>
+export default function Login() {
+  return (
+    <section className="w-full h-screen flex flex-row">
+      <div className="w-[40%] h-full bg-[#8844bc]"></div>
+      <div className="w-[60%] h-full bg-[#fff]"></div>
+    </section>
+  );
 }
