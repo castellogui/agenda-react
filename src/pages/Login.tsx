015b38733193c37d72f34d1a1f5221fae9e2bea3
Guilherme Castello <guilherme.brasolin@outlook.com>
2022-11-08 04:17:04 -0300

    adding morgant font to index.css and logo+span to login screen

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,12 @@
 export default function Login() {
   return (
     <section className="w-full h-screen flex flex-row">
-      <div className="w-[40%] h-full bg-[#8844bc]"></div>
+      <div className="w-[40%] h-full bg-[#6327C8] flex flex-col">
+        <div className="w-[45%] text-center m-auto">
+          <img src="./src/assets/logo.png" alt="logo" className=" px-14 my-8" />
+          <span className="text-white font-[MorgantRegular] text-sm">AGENDA</span>
+        </div>
+      </div>
       <div className="w-[60%] h-full bg-[#fff]"></div>
     </section>
   );
