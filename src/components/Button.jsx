ffc638af5b14a5bb892a3cef3854e85808987805
Guilherme Castello <guilherme.castello@outlook.com>
2022-11-28 12:01:16 -0300

    removing ts

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+export default function Button(props) {
+    return (
+      <div className="p-[0.5rem]">
+        <button
+          type="button"
+          className="bg-[#6327C8] text-white py-1 px-2 transition-all hover:bg-[#352575]"
+        >
+          {props.title}
+        </button>
+      </div>
+    );
+  }
---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx}",
+        "./src/components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            gridTemplateColumns: {
+                'auto-fit': 'repeat(auto-fit, minmax(0, 1fr))',
+                'auto-fill': 'repeat(auto-fill, minmax(0, 1fr))',
+            },
+            gridTemplateRows: {
+                'auto-fit': 'repeat(auto-fit, minmax(0, 1fr))',
+                'auto-fill': 'repeat(auto-fill, minmax(0, 1fr))',
+            },
+        },
+    },
+    plugins: [],
+}
