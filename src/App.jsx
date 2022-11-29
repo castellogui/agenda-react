48ddd4e6d005ebd351aa581e844b511605879c45
Guilherme Castello <guilherme.castello@outlook.com>
2022-11-29 12:29:19 -0300

    adding home path in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import Login from '../src/pages/Login'
+import Home from '../src/pages/Home'
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<Login></Login>} />
+        <Route path='/home' element={<Home></Home>} />
       </Routes>
     </div>    
   )
