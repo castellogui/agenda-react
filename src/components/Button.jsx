445530c960a1a6377702728d0e1167b1bc207959
Guilherme Castello <guilherme.castello@outlook.com>
2022-11-29 12:29:08 -0300

    changing order of props in button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,8 +3,8 @@ export default function Button(props) {
       <div className="p-[0.5rem]">
         <button
           type="button"
-          className="bg-[#6327C8] text-white py-1 px-2 transition-all hover:bg-[#352575]"
           {...props}
+          className="bg-[#6327C8] text-white py-1 px-2 transition-all hover:bg-[#352575] rounded-md"
         >
           {props.title}
         </button>
