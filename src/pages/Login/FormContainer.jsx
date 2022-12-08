bb48484ae6ccd4f67eef8c6cdbae3214ae49733f
Guilherme Brasolin <guilherme.brasolin@g-ushin.com>
2022-12-08 12:01:03 -0300

    applying responsivity

--- src/pages/Login/FormContainer.jsx
+++ src/pages/Login/FormContainer.jsx
@@ -1,7 +1,7 @@
-export default function FormContainer(props){
-    return (
-        <div className="min-w-[25%] w-auto min-h-[50%] h-auto bg-[#e4e4e4] rounded-xl m-auto opacity-80 text-center">
-            {props.children}
-        </div>
-    )
+export default function FormContainer(props) {
+  return (
+    <div className="relative min-w-[25%] w-auto 2xl:w-[25%] min-h-[50%] h-auto bg-[#e4e4e4] rounded-xl my-auto mx-10 md:m-auto py-4 opacity-80 text-center z-50">
+      {props.children}
+    </div>
+  );
 }
