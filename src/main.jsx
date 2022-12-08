bf9b1ff1cbbf1f6340cafb84817468862c1240a7
Guilherme Brasolin <guilherme.brasolin@g-ushin.com>
2022-12-08 13:29:36 -0300

    adding provider in main.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { BrowserRouter } from 'react-router-dom'
-import '../src/styles/index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import "../src/styles/index.css";
+import { Provider } from "react-redux";
+import configStore from "./store/store.config";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-)
+const store = configStore;
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>
+);
