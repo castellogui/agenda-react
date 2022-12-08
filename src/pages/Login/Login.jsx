072ccd004810c60741fd14edd19cdce06c8ed171
Guilherme Brasolin <guilherme.brasolin@g-ushin.com>
2022-12-08 12:28:08 -0300

    finishing login page elements

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,15 @@
-import FormContainer from "./FormContainer";
-import Waves from "./Waves";
+import FormContainer from "./components/FormContainer";
+import Waves from "./components/Waves";
 import styles from "./style.css";
 import AgendaInput from "../../components/AgendaInput";
-import Button from "../../components/AgendaButton";
+import AgendaButton from "../../components/AgendaButton";
+import GoogleButton from "./components/GoogleButton";
+import SectionBreak from "./components/SectionBreak";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const navigate = useNavigate();
+
   return (
     <section className="w-full h-screen flex flex-row bg-gradient-to-t from-[#461999] to-[#020024] relative">
       <FormContainer>
@@ -12,39 +17,21 @@ export default function Login() {
           <span className="block text-3xl font-bold text-[#020024]">Agenda</span>
           <span className="block text-4xl font-light text-[#020024]">Welcome!</span>
         </div>
-        <div className="w-full h-10 flex justify-center my-5">
-          <div
-            id="g_id_onload"
-            data-client_id={import.meta.env.VITE_GOOGLE_CLIENT_ID}
-            data-login_uri="https://your.domain/your_login_endpoint"
-            data-auto_prompt="false"
-          ></div>
-          <div
-            className="g_id_signin"
-            data-type="standard"
-            data-size="large"
-            data-theme="outline"
-            data-text="sign_in_with"
-            data-shape="rectangular"
-            data-logo_alignment="left"
-          ></div>
-        </div>
-        <div className="barrier-container">
-          <div className="barrier">
-            <span>OR LOGIN WITH EMAIL</span>
-          </div>
-        </div>
+        <GoogleButton></GoogleButton>
+        <SectionBreak text="OR LOGIN WITH EMAIL"></SectionBreak>
         <form>
           <AgendaInput label="Email" type={`text`} size="93" margin={`m-auto`}></AgendaInput>
           <AgendaInput label="Password" type={`password`} size="93" margin={`m-auto`}></AgendaInput>
-          <Button title={`Sign In`} customStyle={`rounded-lg py-2 w-[93%]`}></Button>
+          <AgendaButton title={`Sign In`} customstyle={`rounded-lg py-2 w-[93%]`}></AgendaButton>
         </form>
         <span className="block text-lg font-light my-2 text-[#020024]">Forget Password?</span>
-        <div className="barrier-container">
-          <div className="barrier">
-            <span>OR SIGN UP</span>
-          </div>
-        </div>
+        <SectionBreak
+          action={true}
+          actionFunction={() => {
+            navigate("/signup");
+          }}
+          text="OR SIGN UP"
+        ></SectionBreak>
       </FormContainer>
       <Waves />
     </section>
