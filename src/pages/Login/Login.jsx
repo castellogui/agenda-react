72c54a39daf324db23c37a8f3ba361fe34c6e51c
Guilherme Brasolin <guilherme.brasolin@g-ushin.com>
2022-12-08 16:59:40 -0300

    adding handle methods of google login in two ways of render and handle token with email

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -5,10 +5,72 @@ import AgendaInput from "../../components/AgendaInput";
 import AgendaButton from "../../components/AgendaButton";
 import GoogleButton from "./components/GoogleButton";
 import SectionBreak from "./components/SectionBreak";
+import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { connect, useSelector } from "react-redux";
+import { updateName } from "../../store/actions/user";
+import { showMessage } from "../../mixins/MessagesMixin";
 
-export default function Login() {
+function Login() {
   const navigate = useNavigate();
+  const divGoogleButtonRender = useRef();
+  const actualUser = useSelector((state) => state.user);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = (e) => {
+    e.preventDefault();
+    console.log(email);
+    console.log(password);
+  };
+
+  const handleToken = () => {};
+
+  window.handleGoogleToken = (response, error) => {
+    try {
+      if (error == undefined) {
+        //token verify
+        console.log(response);
+      } else {
+        showMessage(
+          "error",
+          undefined,
+          "There was an error!",
+          `Google API returned an error. Error: ${error.message}`,
+          undefined,
+          undefined,
+          "Okay"
+        );
+      }
+    } catch (error) {
+      showMessage(
+        "error",
+        undefined,
+        "There was an error!",
+        `We have an error while trying to verify Google Token in our API. Error: ${error.message}`,
+        undefined,
+        undefined,
+        "Okay"
+      );
+    }
+  };
+
+  useEffect(() => {
+    if (window.google != undefined) {
+      window.google.accounts.id.initialize({
+        client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+        callback: (response, error) => handleGoogleToken(response, error),
+      });
+
+      window.google.accounts.id.renderButton(divGoogleButtonRender.current, {
+        theme: "outline",
+        size: "large",
+        type: "standard",
+        text: "sign_in_with",
+        shape: "rectangular",
+      });
+    }
+  });
 
   return (
     <section className="w-full h-screen flex flex-row bg-gradient-to-t from-[#461999] to-[#020024] relative">
@@ -17,12 +79,32 @@ export default function Login() {
           <span className="block text-3xl font-bold text-[#020024]">Agenda</span>
           <span className="block text-4xl font-light text-[#020024]">Welcome!</span>
         </div>
-        <GoogleButton></GoogleButton>
+        <GoogleButton refDiv={divGoogleButtonRender}></GoogleButton>
         <SectionBreak text="OR LOGIN WITH EMAIL"></SectionBreak>
-        <form>
-          <AgendaInput label="Email" type={`text`} size="93" margin={`m-auto`}></AgendaInput>
-          <AgendaInput label="Password" type={`password`} size="93" margin={`m-auto`}></AgendaInput>
-          <AgendaButton title={`Sign In`} customstyle={`rounded-lg py-2 w-[93%]`}></AgendaButton>
+        <form onSubmit={(e) => signIn(e)}>
+          <AgendaInput
+            label="Email"
+            type={`text`}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            size="93"
+            margin={`m-auto`}
+          ></AgendaInput>
+          <AgendaInput
+            label="Password"
+            type={`password`}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            size="93"
+            margin={`m-auto`}
+          ></AgendaInput>
+          <AgendaButton
+            title={`Sign In`}
+            type={"submit"}
+            customstyle={`rounded-lg py-2 w-[93%]`}
+          ></AgendaButton>
         </form>
         <span className="block text-lg font-light my-2 text-[#020024]">Forget Password?</span>
         <SectionBreak
@@ -37,3 +119,11 @@ export default function Login() {
     </section>
   );
 }
+
+const mapStateToProps = (state) => {
+  return {
+    userName: state.user.name,
+  };
+};
+
+export default connect(mapStateToProps)(Login);
